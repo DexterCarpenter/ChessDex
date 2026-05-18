186ef62a17be49157facbbc93a9009d52db0bb6f
Dexter Carpenter <dexter.carpenter03@gmail.com>
2026-05-17 19:00:45 -0700

    load venv on container startup

--- src/main.py
+++ src/main.py
@@ -1,13 +1,15 @@
+import sys
 from models import bitboard
-from gmpy2 import mpz
+from gmpy2 import xmpz
 
 TestBoard = bitboard.Board()
 
-print(TestBoard.position)
+# print(TestBoard.position)
 
-a = mpz(7)
-bin(a)
-a = a.setbit(48)
-bin(a)
+a = xmpz(7)
+a[0] = 1
+print(bin(a))
+
+print(sys.getsizeof(a))
 
 
